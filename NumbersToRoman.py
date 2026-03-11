6a8da82356780647a6939ec2bfc704081f2733ce
Julszzz <julian.rascone@gmail.com>
2026-03-10 18:59:47 -0700

    Added Description in NumbersToRoman.py

--- NumbersToRoman.py
+++ NumbersToRoman.py
@@ -1,3 +1,5 @@
+# This function returns the Roman equivalent number
+
 def int_to_roman(num):
     roman_map = { 1: 'I', 4: 'IV', 5: 'V', 9: 'IX', 10: 'X', 40: 'XL', 50: 'L', 90: 'XC', 100: 'C', 400: 'CD', 500: 'D', 900: 'CM', 1000: 'M'}
     integers = list(roman_map)
@@ -11,4 +13,4 @@ def int_to_roman(num):
         else:
             i -= 1
     return result
-print(int_to_roman(15))
+print(int_to_roman(35))
