45fb150885f25a93298cb185c77921e179c3eaa6
Julszzz <julian.rascone@gmail.com>
2026-03-12 16:57:31 -0700

    Changed description in pageCount.py

--- pageCount.py
+++ pageCount.py
@@ -1,9 +1,11 @@
-# This function calculates the shortest page flip of a book
-# that is, which way is the fastest to reach a desired page
-# the front of the book or the back
-# p is the desired page and n is the total number of pages in
-# the book
-# |0|1| -> |2|3| -> |4|5| ...
+""""
+This function calculates the shortest page flip of a book
+that is, which way is the fastest to reach a desired page
+the front of the book or the back
+p is the desired page and n is the total number of pages in
+the book
+ |0|1| -> |2|3| -> |4|5| ...
+"""
 
 def pageCount(n, p):
     countLow = p//2
